97eb19f52b48f4f954609b770dbb9db1e9a4494d
RubeMax <KozlovMD.Work@yandex.ru>
2023-03-07 03:41:45 +0700

    Добавлен Auth.js, прописана пробная регистрация (рабочая), осталось доделать дизайн

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -1,6 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import validator from "validator/es";
 export default function Header(){
+    const [register, setRegister] = useState(() => {
+        return {
+            username: "",
+            email: "",
+            password: "",
+            password2: "",
+        }
+    })
+
+    const changeInputRegister = event => {
+        event.persist()
+        setRegister(prev => {
+            return {
+                ...prev,
+                [event.target.name]: event.target.value,
+            }
+        })
+    }
+
+
+    const submitChackin = event => {
+        event.preventDefault();
+        if(!validator.isEmail(register.email)) {
+            alert("You did not enter email")
+        } else if(register.password !== register.password2) {
+            alert("Repeated password incorrectly")
+        } else if(!validator.isStrongPassword(register.password, {minSymbols: 0})) {
+            alert("Password must consist of one lowercase, uppercase letter and number, at least 8 characters")
+        } else {
+            axios.post("http://www.youtube.com", {
+                username: register.username,
+                email: register.email,
+                password: register.password,
+            }).then(res => {
+                if (res.data === true) {
+                    window.location.href = "/auth"
+                } else {
+                    alert("There is already a user with this email")
+                }
+            }).catch(() => {
+                alert("An error occurred on the server")
+            })
+        }
+    }
+
+
     return (
-        <div/>
+        <div className="auth">
+            <h2>Register user:</h2>
+            <form onSubmit={submitChackin}>
+                <p>Name: <input
+                    type="username"
+                    id="username"
+                    name="username"
+                    value={register.usernamr}
+                    onChange={changeInputRegister}
+                /></p>
+                <p>Email: <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={register.email}
+                    onChange={changeInputRegister}
+                    formNoValidate
+                /></p>
+                <p>Password: <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={register.password}
+                    onChange={changeInputRegister}
+                /></p>
+                <p>Repeat password: <input
+                    type="password"
+                    id="password2"
+                    name="password2"
+                    value={register.password2}
+                    onChange={changeInputRegister}
+                /></p>
+                <input type="submit"></input>
+            </form>
+        </div>
     )
 }
+
