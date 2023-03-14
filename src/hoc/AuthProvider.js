702fbdb706a31d1e0ebe2a8b55e5c9206477056b
Кирил Эктоф <kirill.ektoff@yandex.ru>
2023-03-14 20:40:12 +0700

    Edited the loginUser method in AuthProvider.js

--- src/hoc/AuthProvider.js
+++ src/hoc/AuthProvider.js
@@ -1,6 +1,5 @@
 import React, {createContext, useState} from 'react';
 import ky from "ky";
-import {Link} from "react-router-dom";
 import sha256 from 'js-sha256';
 
 
@@ -12,24 +11,19 @@ export const AuthProvider = ({ children }) => {
 
     const loginUser = async (email, password) => {
         password = sha256(password)
-        const result = await ky.get('http://192.168.0.107:7119/api/users?email=' + email + 'password=' + password, {
+        const result = await ky.get('http://192.168.0.107:7119/api/users?email=' + email + '&password=' + password, {
                 headers: {
                     'x-apikey': '59a7ad19f5a9fa0808f11931',
                     'Access-Control-Allow-Origin': '*',
                     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
                 }
             }
-        ).json();
-
-        console.log(result)
-
-        if (result && result.password === password) {
-            setUser(result);
-            setIsLoggedIn(true);
+        )
+        if (result.status === 200) {
+            setUser(result.json())
+            setIsLoggedIn(true)
             alert("Удачная авторизация")
         } else {
-            setIsLoggedIn(false);
-            console.log('Invalid login credentials');
             alert("Неверные данные")
         }
     }
