5b8844423d9c4b6b925ec43ec8caad31b9f484db
RubeMax <KozlovMD.Work@yandex.ru>
2023-03-14 19:00:47 +0700

    Library added sha256, added hashing and password, also changed the line of access to the server

--- src/hoc/AuthProvider.js
+++ src/hoc/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, {createContext, useState} from 'react';
 import ky from "ky";
 import {Link} from "react-router-dom";
+import sha256 from 'js-sha256';
 
 export const AuthContext = createContext({ })
 
@@ -9,7 +10,8 @@ export const AuthProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     const loginUser = async (email, password) => {
-        const result = await ky.get('http://192.168.0.107:7119/api/users?email=' + email, {
+        password = sha256(password)
+        const result = await ky.get('http://192.168.0.107:7119/api/users?email=' + email + 'password=' + password, {
                 headers: {
                     'x-apikey': '59a7ad19f5a9fa0808f11931',
                     'Access-Control-Allow-Origin': '*',
@@ -25,7 +27,7 @@ export const AuthProvider = ({ children }) => {
             setIsLoggedIn(true);
             alert("Удачная авторизация")
         } else {
-            setIsLoggedIn(false); // работает с задержкой, вероятно из-за асинхронности.
+            setIsLoggedIn(false);
             console.log('Invalid login credentials');
             alert("Неверные данные")
         }
